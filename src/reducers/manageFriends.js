387c044fc294eacc3e95d28ca34f4ce6ff1d1fa5
rumikoacopa <rumikorios@gmail.com>
2021-03-02 17:22:56 -0500

    Revert "Done."
    This reverts commit e0964962706741ad539db3dcbc314f7ee521ecff.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,25 +1,2 @@
-export function manageFriends(state={freinds: [], }, action){
-  switch(action.type){
-    case ADD_FRIEND: 
-    return(
-      {...state, 
-        friends: [
-          ...state.friends,
-          action.friend
-        ]
-      }
-    )
-    case "REMOVE_FRIEND":
-      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
-      return (
-        {...state,
-            friends: [
-            ...state.friends.slic(0, removalIndex),
-            ...state.friends.slic(removalIndex + 1)
-          ]
-        }
-      )
-    default: 
-      return state;
-  }
+export function manageFriends(state, action){
 }
